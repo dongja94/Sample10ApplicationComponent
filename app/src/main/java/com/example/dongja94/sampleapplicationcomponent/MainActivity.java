ddfc4c7371c58a4c709ad65d25e5c08afa295d90
dongja94 <dongja94@gmail.com>
2016-01-19 17:48:57 +0900

    startActivityForResult

--- app/src/main/java/com/example/dongja94/sampleapplicationcomponent/MainActivity.java
+++ app/src/main/java/com/example/dongja94/sampleapplicationcomponent/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.dongja94.sampleapplicationcomponent;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText inputView;
-
+    private static final int REQUEST_CODE_OTHER = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +24,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message = inputView.getText().toString();
                 Intent intent = new Intent(MainActivity.this, OtherActivity.class);
-                intent.putExtra("message", message);
-                startActivity(intent);
+                intent.putExtra(OtherActivity.EXTRA_MESSAGE, message);
+                startActivityForResult(intent, REQUEST_CODE_OTHER);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_OTHER) {
+            if (resultCode == Activity.RESULT_OK) {
+                String resultMessage = data.getStringExtra(OtherActivity.RESULT_MESSAGE);
+                Toast.makeText(this, "result : " + resultMessage, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Result Cancel", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dongja94/sampleapplicationcomponent/OtherActivity.java
+++ app/src/main/java/com/example/dongja94/sampleapplicationcomponent/OtherActivity.java
@@ -1,24 +1,31 @@
 package com.example.dongja94.sampleapplicationcomponent;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
 
     TextView messageView;
+    EditText resultView;
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String RESULT_MESSAGE = "result";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
 
         messageView = (TextView)findViewById(R.id.text_message);
+        resultView = (EditText)findViewById(R.id.edit_result);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra("message");
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
 
         messageView.setText(message);
 
@@ -26,6 +33,11 @@ public class OtherActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String result = resultView.getText().toString();
+                // ...
+                Intent data = new Intent();
+                data.putExtra(RESULT_MESSAGE, result);
+                setResult(Activity.RESULT_OK, data);
                 finish();
             }
         });
